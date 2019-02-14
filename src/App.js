00835eb67d41aded0bf459a28d45d0cf914c1005
AlesiaSk <alesya213@gmail.com>
2019-02-14 13:22:15 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,54 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      text:"Hello"
+    };
+  }
+
+  isFormula(){
+    var reg = /^(([A-Z]|[10]|Sub[0-9]*)|(\(([A-Z]|[01]|Sub[0-9]*)([&\|~]|->)([A-Z]|[01]|Sub[0-9]*)\))|\(!([A-Z]|[10]|Sub[0-9]*)\))$/;
+    var reg3 = /(\(([A-Z]|[01]|Sub[0-9]*)([&\|~]|->)([A-Z]|[01]|Sub[0-9]*)\))|\(!([A-Z]|[10]|Sub[0-9]*)\)/;
+    console.log(reg.test(this.refs.textBox.value));
+    var formula = this.refs.textBox.value;
+    var subsArray;
+    var ind = 0;
+      while(reg3.test(formula)){
+        subsArray = formula.match(/(\(([A-Z]|[01]|Sub[0-9]*)([&\|~]|->)([A-Z]|[01]|Sub[0-9]*)\))|\(!([A-Z]|[10]|Sub[0-9]*)\)/g);
+        console.log(subsArray);
+        for(var i=0; i<subsArray.length; i++){
+         formula = formula.replace(subsArray[i], "Sub"+ind);
+         ind++;
+        }
+        
+        console.log(formula);
+    }
+    
+    if(reg.test(formula)){
+      alert("It's a formula");
+    }
+
+    else{
+      alert("It's not a formula");
+    }
+    
+  } 
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="app">
+        <h1>Enter a logic formula</h1>
+         <input ref="textBox" type="text"/>
+         <button onClick={ (e) => {this.isFormula();}}>Submit</button>
       </div>
     );
   }
+  
 }
+
 
 export default App;
