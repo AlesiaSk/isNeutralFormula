541ca8e2ccd0dc2fc9d6073b73eb8e959316b166
AlesiaSk <alesya213@gmail.com>
2019-02-14 13:27:39 +0300

    Added author and deleted console.log

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
 
+/*
+Author - Skorokhod Alesya
+*/
+
 class App extends Component {
 
   constructor(){
@@ -13,19 +17,15 @@ class App extends Component {
   isFormula(){
     var reg = /^(([A-Z]|[10]|Sub[0-9]*)|(\(([A-Z]|[01]|Sub[0-9]*)([&\|~]|->)([A-Z]|[01]|Sub[0-9]*)\))|\(!([A-Z]|[10]|Sub[0-9]*)\))$/;
     var reg3 = /(\(([A-Z]|[01]|Sub[0-9]*)([&\|~]|->)([A-Z]|[01]|Sub[0-9]*)\))|\(!([A-Z]|[10]|Sub[0-9]*)\)/;
-    console.log(reg.test(this.refs.textBox.value));
     var formula = this.refs.textBox.value;
     var subsArray;
     var ind = 0;
       while(reg3.test(formula)){
         subsArray = formula.match(/(\(([A-Z]|[01]|Sub[0-9]*)([&\|~]|->)([A-Z]|[01]|Sub[0-9]*)\))|\(!([A-Z]|[10]|Sub[0-9]*)\)/g);
-        console.log(subsArray);
         for(var i=0; i<subsArray.length; i++){
          formula = formula.replace(subsArray[i], "Sub"+ind);
          ind++;
         }
-        
-        console.log(formula);
     }
     
     if(reg.test(formula)){
